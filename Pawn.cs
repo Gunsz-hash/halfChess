38a42507604e5c679c60f0bb307d8bbf3faafb2f
ItsTheHadad <Amit.hadad@s.afeka.ac.il>
2024-11-14 21:04:25 +0200

    changed the game logic, added a few classes

---
+++ Pawn.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalProject
+{
+    internal class Pawn : Piece
+    {
+
+
+        public Pawn(PieceColor color, Position position) : base(color, PieceType.Pawn, position) { }
+
+        public override bool IsValidMove(Position startPoint, Position endPoint, Board board)
+        {
+
+            int rowDiffernce = Math.Abs(endPoint.Row - startPoint.Row);
+            int colDiffernce = Math.Abs(endPoint.Col - startPoint.Col);
+
+
+            if (rowDiffernce + colDiffernce > 2)
+            {
+                return false;
+            }
+
+
+            // forward moves (also backwards, but disabled)
+
+            else if ((rowDiffernce == 0 && colDiffernce == 0) || rowDiffernce > 2 || colDiffernce > 1) // (start and end are the same) or if more than 2 rows or 1 col
+            {
+                return false;
+            }
+
+            else if (rowDiffernce > 1 && colDiffernce == 0) //if i can use a double "jump"
+            {
+                if (this.Color == PieceColor.Black)
+                {
+                    if (this.Position.Row != 1)
+                    {
+                        return false;
+                    }
+
+                    else
+                    {
+                        return CheckDirectionRow(startPoint, endPoint); ;
+                    }
+
+                }
+                else
+                {
+                    if (this.Position.Row != 6)
+                    {
+                        return false;
+                    }
+
+                    else
+                    {
+                        return CheckDirectionRow(startPoint, endPoint); ;
+                    }
+                }
+
+
+            }
+
+            else if (rowDiffernce == 1 && colDiffernce == 0) //regular forward move
+            {
+                return CheckDirectionRow(startPoint, endPoint);
+            }
+
+            // horizontal moves
+
+            else if (rowDiffernce == 0 && colDiffernce == 1)
+            {
+                return true;
+            }
+
+            //diagonal moves
+
+            else if(rowDiffernce == 1 && colDiffernce == 1)
+            {
+                return CheckDirectionRow(startPoint,endPoint);
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+
+        public bool CheckDirectionRow(Position start, Position end)
+        {
+            if (this.Color == PieceColor.Black)
+            {
+                if (end.Row > start.Row)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+            else
+            {
+                if (end.Row < start.Row)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+
+
+
+
+    }
+}
---
+++ Piece.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalProject
+{
+    public enum PieceType {Empty, King, Rook, Bishop, Knight, Pawn}
+    public enum PieceColor {None, White, Black}
+
+
+    internal abstract class Piece
+    {
+        public PieceColor Color { get; set; }
+        public PieceType Type { get; private set;}
+        public Position Position { get; private set;}
+
+        protected Piece(PieceColor color, PieceType type, Position position)
+        {
+            Color = color;
+            Type = type;
+            //if(position.InBounds()
+            //throw if not;
+            Position = position;
+        }
+
+
+
+        public abstract bool IsValidMove(Position startPoint, Position endPoint, Board board);
+
+        public bool IsWhite()
+        {
+            return Color == PieceColor.White;
+        }
+
+        public void SetPosition(Position newPosition)
+        {
+            //if(!newPosition.InBounds) todo fix inbound - if not, throw 
+            
+            //add else clause later;
+            Position = newPosition;
+
+        }
+
+       
+    }
+}
